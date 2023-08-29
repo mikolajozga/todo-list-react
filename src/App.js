0e382f2aed621f27cd27d20b51d87aa931144526
Mikolaj Ozga <mikolaj.ozga03@gmail.com>
2023-08-29 20:09:33 +0200

    localStorage added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -8,11 +8,17 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false)
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść obiad", done: true },
-  ]);
+  
+  const localStorageTasks = localStorage.getItem("tasks");
+  
+  const [tasks, setTasks] = useState(
+    localStorageTasks !== null ? JSON.parse(localStorageTasks) : []
+  );
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
